4437589852d5d1d3b95cb61e6cbe7656c85bd2c0
Onur Tanrıkulu <onurtnrkl@gmail.com>
2024-12-02 17:26:56 +0300

    test: add sandbox projects

---
+++ sandbox/GodotApplication/TestSprite.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace GodotApplication;
+
+public partial class TestSprite : Sprite2D
+{
+	[Export]
+	private float _rotationSpeed = 1f;
+
+	public override void _Process(double delta)
+	{
+		Rotate((float)(_rotationSpeed * delta));
+	}
+}
